2931a22319dab55670366a8efe95de9a05e2d457
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2025-07-06 15:40:44 +0900

    Update links.ts

--- utils/links.ts
+++ utils/links.ts
@@ -2,4 +2,6 @@ import {ref} from "vue";
 
 export const nav_links = ref<{ label: string; to: string }[]>([
     { label: 'Home', to: '/' },
+    { label: 'fabric01', to: '/fabric01' },
+    { label: 'fabric02', to: '/fabric02' },
 ])
